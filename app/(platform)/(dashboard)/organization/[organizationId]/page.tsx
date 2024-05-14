84641428813fa28ed45c0ad977d56d7e0589181e
Du Cheng <easyhoo2426@163.com>
2024-05-15 01:36:03 +0800

    test: prisma connect

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import { db } from '~/lib/db'
 
 interface OrganizationIdPageProps {
   params: { organizationId: string }
 }
 
+const create = async (formData: FormData) => {
+  'use server'
+
+  const title = formData.get('title')
+  const res = await db.board.findMany()
+  console.log(title, res)
+}
+
 const OrganizationIdPage = ({
   params: { organizationId }
 }: OrganizationIdPageProps) => {
-  return <div>OrganizationIdPage : {organizationId}</div>
+  return (
+    <div>
+      OrganizationIdPage : {organizationId}
+      <form action={create}>
+        <label htmlFor="title">
+          <input type="text" name="title" className="border" />
+        </label>
+      </form>
+    </div>
+  )
 }
 
 export default OrganizationIdPage
