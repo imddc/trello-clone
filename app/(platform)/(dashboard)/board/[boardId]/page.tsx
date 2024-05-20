7c5cc56d18d419de778f1b2ecde0562c13016bcb
Du Cheng <easyhoo2426@163.com>
2024-05-20 10:18:28 +0800

    feat(board): add card list query

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,11 +1,37 @@
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import { db } from '~/lib/db'
+
 interface BoardIdProsp {
   params: {
-    BoardId: string
+    boardId: string
   }
 }
 
-const BoardIdPage = async ({}: BoardIdProsp) => {
-  return <div>Board Id Page</div>
+const BoardIdPage = async ({ params }: BoardIdProsp) => {
+  const { orgId } = auth()
+  if (!orgId) {
+    redirect('/select-org')
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId
+      }
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: 'asc'
+        }
+      }
+    }
+  })
+  console.log(lists)
+
+  return <div>Board Id Page {params.boardId}</div>
 }
 
 export default BoardIdPage
