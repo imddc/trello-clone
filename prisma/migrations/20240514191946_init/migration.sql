35d309d9a991ae692af177aa9f94aaed33e647da
Du Cheng <easyhoo2426@163.com>
2024-05-20 10:00:10 +0800

    feat(prisma): new schema

--- prisma/migrations/20240514191946_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `updateAt` to the `Board` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Board` ADD COLUMN `createAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    ADD COLUMN `updateAt` DATETIME(3) NOT NULL;
